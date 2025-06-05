bc115b8fff115fa4187b9a5f62c9667cba3605ec
yoochanhan <yoochan.han@stu.ocsb.ca>
2025-06-05 17:56:41 +0000

    added game scene

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python 3
+
+"""
+Created by: Yoochan Han
+Created on: Jun 2025
+This program is the "Space Aliens" program on the PyBadge
+"""
+
+import stage
+import ugame
+
+
+def game_scene() -> None:
+    """This function is the main game game_scene"""
+
+    print("\n\n\n")  # 3 black lines
+    print("hello, World!")
+
+    #  loop
+    while True:
+        pass  # We don use yet
+
+if __name__ == "__main__":
+    game_scene
